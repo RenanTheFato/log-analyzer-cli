4434d3098eb1aa490ab41f0e7b211ef90569b206
RenanTheFato <renan.santana007@hotmail.com>
2026-02-23 15:11:58 -0300

    add the callable from root, turn terminal unnique calls into an CLI like and put assistance commands

--- cli/src/main/java/com/loganalyzercli/Main.java
+++ cli/src/main/java/com/loganalyzercli/Main.java
@@ -2,13 +2,34 @@ package com.loganalyzercli;
 
 import java.util.Scanner;
 
+import com.loganalyzercli.commands.HelloCommand;
 import com.loganalyzercli.commands.RootCommand;
 
 import picocli.CommandLine;
 
 public class Main {
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
-		CommandLine cmd = new CommandLine(new RootCommand());
+
+		CommandLine cmd = new CommandLine(new RootCommand()).addSubcommand("hello", new HelloCommand());
+
+		while (true) {
+			String line = scanner.nextLine().trim();
+
+			if (line.isEmpty()) {
+				continue;
+			}
+
+			if (line.equalsIgnoreCase("exit")) {
+				System.out.println("");
+				break;
+			}
+
+			String[] tokens = line.split("\\s+");
+			cmd.execute(tokens);
+
+		}
+		scanner.close();
 	}
 }
