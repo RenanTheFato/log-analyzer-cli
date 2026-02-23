f867fc020815d6578f1c11a6514fcaa85d395406
RenanTheFato <renan.santana007@hotmail.com>
2026-02-23 19:25:10 -0300

    add filter params

--- cli/src/main/java/com/loganalyzercli/commands/LogCommand.java
+++ cli/src/main/java/com/loganalyzercli/commands/LogCommand.java
@@ -13,11 +13,17 @@ import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "log", mixinStandardHelpOptions = true)
+@Command(name = "log", mixinStandardHelpOptions = true, version = "ALPHA 0.0.1")
 public class LogCommand implements Callable<Integer> {
 
-  @Option(names = { "-f", "--file" }, paramLabel = "FILE", description = "Path where log file is", required = true)
+  @Option(names = { "-f", "--file" }, paramLabel = "FILE", description = "Path to the log file to be analyzed.", required = true)
   private File file;
+
+  @Option(names = {"-l", "--level"}, paramLabel = "STRING", description = "Filters rows by log level.")
+  private String level;
+
+  @Option(names = {"-wl", "--wordlist"}, paramLabel = "LIST", split = ",", description = "Keywords to filter rows. Can be entered multiple times.")
+  private List<String> wordList;
 
   @Override
   public Integer call() throws Exception {
@@ -49,6 +55,8 @@ public class LogCommand implements Callable<Integer> {
     System.out.printf("│ %-19s │ %-28d │%n", "Lines", lineCount);
     System.out.printf("│ %-19s │ %-28d │%n", "Characters", charCount);
     System.out.printf("│ %-19s │ %-28s │%n", "Last Modified", formattedDate);
+    System.out.printf("│ %-19s │ %-28s │%n", "Log Level", level);
+    System.out.printf("│ %-19s │ %-28s │%n", "Word List", wordList);
     System.out.println("└─────────────────────┴──────────────────────────────┘");
 
     return 0;
